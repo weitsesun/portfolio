9727c568350dd1a0ebf5304ddbd9c86107bf5645
Wayne Sun <wtse.sun@gmail.com>
2019-09-29 21:29:15 -0700

    designed profile box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <div className="App">
-      Portfolio
+      <Profile />
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default function Profile() {
   return (
     <div className="pfrofile">
-      
+      <UpperLeftBox />
+      <ProfileGrid className="profile-picture">
+        <ProfilePicture />
+      </ProfileGrid>
     </div>
   )
 }
+
+const UpperLeftBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background: hsl(250, 100%, 50%);
+`
+
+const ProfileGrid = styled.div`
+  position: relative;
+  top: -70px;
+  left: 30px;
+  width: 30%;
+  height: 90vh;
+  background: hsl(200, 100%, 50%);
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`
+
+const ProfilePicture = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 3px white solid;
+  border-radius: 50%;
+`
