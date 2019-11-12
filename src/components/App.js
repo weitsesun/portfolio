37beab40d32d07de75388060c10c29ab454d197f
Wayne Sun <wtse.sun@gmail.com>
2019-11-11 18:39:24 -0800

    delete useState in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Profile from './Profile'
 import ProjectRender from './ProjectRender'
 import PageCalculator from './calculator/PageCalculator'
