3874e86b4911ea26a7f5bdf7932f7bfac7715839
Wayne Sun <wtse.sun@gmail.com>
2020-02-10 17:08:16 -0800

    created project data

---
+++ src/projectData.js
@@ -0,0 +1,44 @@
+export const projectData = [
+  {
+    name: 'Open Source Table',
+    description: 'Restaurant Search / Reservation Full-Stack Web Application',
+    github: 'github.com/opensource-table',
+    techStack: [
+      'React.js',
+      'Node.js',
+      'Express',
+      'PostgresSQL',
+      'Cassandra',
+      'AWS EC2',
+      'NGINX'
+    ]
+  },
+  {
+    name: 'Hacker Home',
+    description: 'Hotel Search / Reservation Full-Stack Web Application',
+    github: 'github.com/hacker-home',
+    techStack: [
+      'React.js',
+      'Node.js',
+      'Express',
+      'AWS EC2',
+      'MongoDB',
+      'Docker',
+      'Enzyme',
+      'Jest',
+    ]
+  },
+  {
+    name: 'Mobile Step Counter',
+    description: 'Step Counter Full-Stack Mobile Application',
+    github: 'github.com/weitsesun/Mobile-Step-Counter',
+    techStack: [
+      'Javascript',
+      'React-Native',
+      'React.js',
+      'Express',
+      'MongoDB',
+      'AWS EC2',
+    ]
+  },
+]
