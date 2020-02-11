2c4c32d45616b73ce91f547982cf32c1da10e837
Wayne Sun <wtse.sun@gmail.com>
2020-02-10 17:08:35 -0800

    created project render function

--- src/components/ProjectRender.js
+++ src/components/ProjectRender.js
@@ -1,26 +1,53 @@
 import React from 'react'
+import SingleProject from './SingleProject'
 import styled from 'styled-components'
+import { projectData } from '../projectData'
 
 export default function ProjectRender() {
   return (
     <ProjectContainer>
       <ProjectHeader>My Recent Projects</ProjectHeader>
+      <Projects>
+        {projectData.map(project =>
+          <SingleProject project={project} />
+        )}
+      </Projects>
     </ProjectContainer>
   )
 }
 
 const ProjectContainer = styled.div`
+  margin: 0;
+  padding: 0;
   width: 100%;
   height: 100vh;
   display: flex;
-  align-content: center;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
+  /* border-top: 20px solid black; */
+  /* background: linear-gradient(90deg, white 70%, black 70%); */
 `
 
 const ProjectHeader = styled.h1`
+  margin: 0;
   font-family: 'Roboto', sans-serif;
   font-size: 2.2rem;
   font-weight: bold;
   color: black;
-  height: 20%;
+  height: 15vh;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  /* background: yellow; */
+`
+
+const Projects = styled.div`
+  height: 85vh;
+  width: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
 `
