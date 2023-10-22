7ce8f559ed7b1883095b6d25e1e529d8f1b3631a
Wei-Tse Sun <wtse.sun@gmail.com>
2023-10-22 10:32:05 -0700

    migrate to react 18

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,14 @@
-import React, { useRef } from 'react'
-import '../styles/App.css';
-import Logo from './Logo';
+import React from 'react'
+import '../styles/App.css'
+import Logo from './Logo'
 import Greetings from './Greetings'
-import ArrowDown from './ArrowDown'
-import Skills from './Skills'
-import Projects from './Projects'
 
 export default function App() {
-  const arrowTarget = useRef(null);
-
   return (
-    <div className="main">
-      <div className="landing-page">
-        <Logo/>
+    <div className='main'>
+      <div className='landing-page'>
+        <Logo />
         <Greetings />
-        <ArrowDown arrowTarget={arrowTarget}/>
-      </div>
-
-      <div ref={arrowTarget} className="skills-page" >
-        <Skills />
-      </div>
-
-      <div className="projects-page">
-        <Projects />
       </div>
     </div>
   )
